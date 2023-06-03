dbd8f6aaa566a464d59937cf9f3ad2cfd0fe38c2
Guilherme Komninakis <92609086+guikomninakis@users.noreply.github.com>
2023-06-03 00:11:05 -0300

    update
    update

---
+++ HELPTECH-main/help-tech1/README.md
@@ -0,0 +1,3 @@
+# Site Help Tech
+
+
---
+++ HELPTECH-main/help-tech1/telaLogin.js
@@ -0,0 +1,30 @@
+const mode = document.getElementById('mode_icon');
+
+mode.addEventListener('click', () => {
+    const form = document.getElementById('login_form');
+
+    if(mode.classList.contains('fa-moon')) {
+        mode.classList.remove('fa-moon');
+        mode.classList.add('fa-sun');
+
+        form.classList.add('dark');
+        return ;
+    }
+    
+    mode.classList.remove('fa-sun');
+    mode.classList.add('fa-moon');
+
+    form.classList.remove('dark');
+});
+
+function menuShow() {
+    let menuMobile = document.querySelector('.mobile-menu');
+    if (menuMobile.classList.contains('open')) {
+        menuMobile.classList.remove('open');
+        document.querySelector('.icon').src = "menu_white_36dp.svg";
+    } else {
+        menuMobile.classList.add('open');
+        document.querySelector('.icon').src = "close_white_36dp.svg";
+    }
+  } 
+  
